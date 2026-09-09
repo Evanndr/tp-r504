c316867039dc0d9f44210d7ff203d7c92d8bb0e3
Evanndr <evannodari@gmail.com>
2026-09-09 17:33:48 +0200

    Suite du TP (Question 3.4)

--- prog1.py
+++ prog1.py
@@ -1,1 +1,14 @@
+import fonctions as f
 print("Hello, World!")
+
+
+def carre(chiffre):
+	return int(chiffre)**2
+
+while True:
+	chiffre = input("Choisir un nombre pour avoir son carré. ")
+	print(carre(chiffre))
+	a = input("Choisir un nombre a. ")
+	b = input("Choisir un nombre b. ")
+	res = f.puissance(a,b)
+	print(res)
