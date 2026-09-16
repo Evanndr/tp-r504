36822e17817386b9db6219b1863688473aeb4870
Evanndr <evannodari@gmail.com>
2026-09-16 09:04:16 +0200

    Changes to be committed: 	modified:   fonctions.py

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,5 +3,7 @@ def puissance(a, b):
 		raise TypeError("Only integers are allowed")
 	if not type(b) is int:
 		raise TypeError("Only integers are allowed")
+		if b < 0:
+			raise TypeError("opération d’élevation de puissance indéfinie")
 	return a**b
 
