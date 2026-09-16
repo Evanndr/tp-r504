0daabf12736c3cebdffbe631ef42fa397e3f1b4f
Evanndr <evannodari@gmail.com>
2026-09-16 09:24:12 +0200

    Changes to be committed: 	modified:   fonctions.py

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -5,22 +5,17 @@ def puissance(a, b):
 	if not type(b) is int:
 		raise TypeError("Only integers are allowed")
 
-    # Gestion du cas particulier de l'exposant nul
-    if b == 0:
-        return 1
-    
-    # On travaille d'abord avec la valeur absolue de l'exposant
-    exposant_positif = abs(b)
+    # Traiter le cas de l'exposant négatif pour la boucle range
+    exposant = abs(b)
     resultat = 1
     
-    # Boucle pour multiplier 'a', 'b' fois
-    for _ in range(exposant_positif):
-        resultat = resultat * a
+    # On multiplie 'resultat' par 'a', exactement 'exposant' fois
+    for i in range(exposant):
+        resultat = resultat * a     # Et NON PAS a = a * a
         
-    # Si l'exposant de départ était négatif, on inverse le résultat
+    # Si l'exposant était négatif, on applique l'inversion
     if b < 0:
         return 1 / resultat
         
     return resultat
-
 
