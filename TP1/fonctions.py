44ac461bf647ce0e751df7cda620961c3716760c
Evanndr <evannodari@gmail.com>
2026-09-16 09:19:15 +0200

    Changes to be committed: 	modified:   fonctions.py

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -5,7 +5,10 @@ def puissance(a, b):
 		raise TypeError("Only integers are allowed")
 		if b < 0:
 			raise TypeError("opération d’élevation de puissance indéfinie")
-	for i in range(b-1):
+	for i in range(b):
 		a = a*a
+    if b < 0:
+        return 1 / resultat
+
 	return a
 
