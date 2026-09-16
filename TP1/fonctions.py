4fbd29d7754bb7afb0628fcfcc03a695b5fbfad6
Evanndr <evannodari@gmail.com>
2026-09-16 09:27:24 +0200

    Changes to be committed: 	modified:   fonctions.py

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,21 +1,13 @@
 def puissance(a, b):
-
 	if not type(a) is int:
 		raise TypeError("Only integers are allowed")
 	if not type(b) is int:
 		raise TypeError("Only integers are allowed")
-
-    # Traiter le cas de l'exposant négatif pour la boucle range
 	exposant = abs(b)
 	resultat = 1
-    
-    # On multiplie 'resultat' par 'a', exactement 'exposant' fois
 	for i in range(exposant):
-		resultat = resultat * a     # Et NON PAS a = a * a
-        
-    # Si l'exposant était négatif, on applique l'inversion
+		resultat = resultat * a 
 	if b < 0:
  		return 1 / resultat
-        
 	return resultat
 
